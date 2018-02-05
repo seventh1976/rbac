b6c0384ee65e2f27a4fb50e2cc29aac66a3612af
seventh1976 <seventh1976@gmail.com>
2018-02-05 13:38:34 +0800

    normalize input for better internal use

--- rbac-class.js
+++ rbac-class.js
@@ -1,13 +1,34 @@
 class RBAC {
   constructor(roles) {
+    this.init(opts);
+  }
+
+  init(roles) {
     if(typeof roles !== 'object') {
       throw new TypeError('Expected an object as input');
     }
-    this.roles = roles;
-  }
 
-  can(role, operation) {
-    return this.roles[role] && this.roles[role].can.indexOf(operation) !== -1;
+    this.roles = roles;
+    let map = {};
+    Object.keys(roles).forEach(role => {
+      map[role] = {
+        can: {}
+      };
+      if(roles[role].inherits) {
+        map[role].inherits = roles[role].inherits;
+      }
+
+      roles[role].can.forEach(operation => {
+        if(typeof operation === 'string') {
+          map[role].can[operation] = 1;
+        } else if(typeof operation.name === 'string'
+            && typeof operation.when === 'function') {
+            map[role].can[operation.name] = operation.when;
+        }
+      });
+    });
+
+    this.roles = map;
   }
 }
 
